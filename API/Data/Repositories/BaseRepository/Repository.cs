8b892ac43126211bd8a66864e982f84294bc7807
CometArao <cometarao@gmail.com>
2025-05-21 15:52:42 -0400

    FEAT: Implmenetation of BaseRepository to avoid boiler-plate code; working only with Catalog FTM

---
+++ API/Data/Repositories/BaseRepository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using API.Data.Repositories.BaseRepository;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _ctx;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(ApplicationDbContext ctx)
+    {
+        _ctx = ctx;
+        _dbSet = ctx.Set<T>();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+        => await _dbSet.FindAsync(id);
+
+    public async Task<IEnumerable<T>> GetAllAsync(int skip = 0, int take = 50)
+        => await _dbSet
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync();
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _ctx.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _ctx.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await _ctx.SaveChangesAsync();
+    }
+}
